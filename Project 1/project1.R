a295c9406e825c46b5a4bd7e0c349416f4087d87
benhendrick <bahendri@mtu.edu>
2016-01-25 02:55:35 -0500

    Update Jan 25 2:55am
    HTML and CSS functional

--- Project 1/project1.R
+++ Project 1/project1.R
@@ -1,30 +1,108 @@
-install.packages("ggplot2")
-library(ggplot2)
+adultdata <- read.csv("~/Desktop/adultdata.txt")
+
 # 2.b.i
-adulttest[adulttest == " ?"] <- NA
-apply(is.na(adulttest),2,sum)/nrow(adulttest)*100
+adultdata[adultdata == " ?"] <- NA
+apply(is.na(adultdata),2,sum)/nrow(adultdata)*100
 
 # 2.b.ii
-hist(apply(is.na(adulttest),1,sum),
+hist(apply(is.na(adultdata),1,sum),
      xlab = "Number of Missing Values Per Row",
      ylab = "Frequency",
      main = "Number of Missing Values",
      breaks = 3,
      col = "blue")
 # 2.c
-adultnumeric <- adulttest[,c(1,3,5,11,12,13)]
-adultnumeric[is.na(adultnumeric)] <- 0
+adultnumeric <- adultdata[,c(1,3,5,11,12,13)]
+adultcat <- adultdata[,-c(1,3,5,11,12,13)]
+names(adultnumeric) # numeric
+names(adultcat) # chategorical
 
 # 2.d.i
-for(i in 1:length(adultnumeric)){
-  ifelse(length(unique(adultnumeric[,i])) < 100,
-         hist(adultnumeric[,i], 
-              xlab = names[i],
-              breaks = length(unique(adultnumeric[,i]))),
-         hist(adultnumeric[,i], breaks = 100))
-}
+apply(adultnumeric, 2, function(x) ifelse(length(unique(x)) < 100,
+              hist(x, breaks = length(unique(x))),
+              hist(x, breaks = 100)))
 
 # 2.d.ii
+lesser <- adultdata[which(adultdata$income == " <=50K"), ]
+greater <- adultdata[which(adultdata$income == " >50K"), ]
+
+for(i in c(1,3,5,11,12,13)){
+par(mfrow=c(1,2))
+hist(lesser[,i],
+     xlab = names(lesser)[i],
+     ylab = "Frequency",
+     main = "",
+     col = "blue")
+hist(greater[,i],
+     xlab = names(greater)[i],
+     ylab = "Frequency",
+     main = "",
+     col = "red")
+}
+
+#2.d.iii
+for(i in c(1,3,5,11,12,13)){
+  par(mfrow=c(1,2))
+  boxplot(lesser[,i],
+       xlab = names(lesser)[i],
+       ylab = "Frequency",
+       main = "",
+       col = "blue")
+  boxplot(greater[,i],
+       xlab = names(greater)[i],
+       ylab = "Frequency",
+       main = "",
+       col = "red")
+}
+
+par(mfrow = c(1,1))
+
+#2.d.iv
+
+#2.e.i
+for(i in c(2,4,6,7,8,9,10,14,15)){
+barplot(table(adultdata[,i]), 
+        las = 2, 
+        cex.names = 0.65,
+        main = "",
+        xlab = names(adultcat)[i],
+        col = "blue")
+}
+
+#2.3.ii
+for(i in c(2,4,6,7,8,9,10,14,15)){
+  par(mfrow=c(1,2))
+  barplot(table(lesser[,i]), 
+          las = 2, 
+          cex.names = 0.65,
+          main = "",
+          xlab = names(lesser)[i],
+          col = "blue")
+  barplot(table(greater[,i]), 
+          las = 2, 
+          cex.names = 0.65,
+          main = "",
+          xlab = names(greater)[i],
+          col = "red")
+}
+
+par(mfrow = c(1,1))
+
+#2.f.i
+plot(y = adultdata$education.num, x = adultdata$age,
+     xlab = "Age",
+     ylab = "Education",
+     main = "Education vs. Age")
+
+cor(adultdata$education.num, adultdata$age)
+
+#2.f.i
+plot(x = adultnumeric$capital.gain, y = adultnumeric$hours.per.week,
+     xlab = "Capital Gain",
+     ylab = "Hours Per Week",
+     main = "Capital Gain vs. Hours Per Week")
+
+cor(adultnumeric$capital.gain, adultnumeric$hours.per.week)
 
 # 3.a
 nfl.passing.2014 <- read.csv("~/Google Drive/Academic/Spring 2016/CS 4821/nfl-passing-2014.csv")
@@ -37,6 +115,8 @@ summary(nfl.passing.2014$Int)
 names(sort(-table(nfl.passing.2014$Int)))[1]
 
 #3.a.ii
+quantile(nfl.passing.2014$Yds)
+quantile(nfl.passing.2014$Rate)
 
 #3.a.iii
 fivenum(nfl.passing.2014$Cmp.)
@@ -78,13 +158,10 @@ plot(x = nfl.passing.2014$TD,
 tennis <- read.csv("~/Google Drive/Academic/Spring 2016/CS 4821/2014w.csv")
 
 #3.b.i
-top5 <- tennis[tennis$Winner %in% names(head(sort(table(tennis$Winner), 
-                                                  decreasing = TRUE))), ]
-W1 <- tennis$W1[which(!is.na(tennis$W1))]
-W2 <- tennis$W2[which(!is.na(tennis$W2))]
-W3 <- tennis$W3[which(!is.na(tennis$W3))]
+twenty <- tennis[]
 
-mean(c(W1,W2,W3))
+top5 <- tennis[tennis$Winner %in% names(head(sort(table(tennis$Winner), 
+                                                  decreasing = TRUE),n=5)), ]
 
 #3.c
 wc.data.2014 <- read.csv("~/Google Drive/Academic/Spring 2016/CS 4821/wc-data-2014.csv")
@@ -104,7 +181,3 @@ sort(tapply(groupStage$home_dif, groupStage$home_continent, FUN=sum) +
      decreasing = TRUE)
 
 #3.c.iii
-
-
-
-
