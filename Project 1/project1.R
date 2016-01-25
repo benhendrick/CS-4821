2245ebd1deb1ee71b0140f3a32435c5398c8b381
benhendrick <bahendri@mtu.edu>
2016-01-24 22:33:19 -0500

    Update 1/24 10:32pm
    Completed problems 2 and 3 (excluding 3.c.iii)

--- Project 1/project1.R
+++ Project 1/project1.R
@@ -30,36 +30,35 @@ for(i in 1:length(adultnumeric)){
 nfl.passing.2014 <- read.csv("~/Google Drive/Academic/Spring 2016/CS 4821/nfl-passing-2014.csv")
 
 #3.a.i
-mean(nfl.passing.2014$TD)
-median(nfl.passing.2014$TD)
+summary(nfl.passing.2014$TD)
 names(sort(-table(nfl.passing.2014$TD)))[1]
 
-mean(nfl.passing.2014$Int)
-median(nfl.passing.2014$Int)
+summary(nfl.passing.2014$Int)
 names(sort(-table(nfl.passing.2014$Int)))[1]
 
 #3.a.ii
-quantile(nfl.passing.2014$Yds)
-quantile(nfl.passing.2014$Rate)
 
 #3.a.iii
-a <- fivenum(nfl.passing.2014$Cmp.)
-b <- fivenum(nfl.passing.2014$Yds)
+fivenum(nfl.passing.2014$Cmp.)
+fivenum(nfl.passing.2014$Yds)
 
 #3.a.iv
 hist(nfl.passing.2014$Yds,
      xlab = "Passing Yards",
      main = "Histogram of Passing Yards",
+     col = "blue",
      breaks = 4)
 
 hist(nfl.passing.2014$Yds,
      xlab = "Passing Yards",
      main = "Histogram of Passing Yards",
+     col = "blue",
      breaks = 8)
 
 hist(nfl.passing.2014$Yds,
      xlab = "Passing Yards",
      main = "Histogram of Passing Yards",
+     col = "blue",
      breaks = 12)
 
 #3.a.v
@@ -76,15 +75,35 @@ plot(x = nfl.passing.2014$TD,
      main = "Passing Touchdowns vs. Interceptions")
 
 #3.b
-'2014w' <- read.csv("~/Google Drive/Academic/Spring 2016/CS 4821/2014w.csv")
+tennis <- read.csv("~/Google Drive/Academic/Spring 2016/CS 4821/2014w.csv")
 
 #3.b.i
+top5 <- tennis[tennis$Winner %in% names(head(sort(table(tennis$Winner), 
+                                                  decreasing = TRUE))), ]
+W1 <- tennis$W1[which(!is.na(tennis$W1))]
+W2 <- tennis$W2[which(!is.na(tennis$W2))]
+W3 <- tennis$W3[which(!is.na(tennis$W3))]
+
+mean(c(W1,W2,W3))
 
 #3.c
 wc.data.2014 <- read.csv("~/Google Drive/Academic/Spring 2016/CS 4821/wc-data-2014.csv")
 
 #3.c.i
-wc.data.2014$home_score - wc.data.2014$away_score
+groupStage <- wc.data.2014[1:48,]
+groupStage$home_dif <- groupStage$home_score - groupStage$away_score
+groupStage$away_dif <- groupStage$away_score - groupStage$home_score
+
+sort(tapply(groupStage$home_dif, groupStage$home, FUN=sum) + 
+       tapply(groupStage$away_dif, groupStage$away, FUN=sum),
+     decreasing = TRUE)
+
+#3.c.ii
+sort(tapply(groupStage$home_dif, groupStage$home_continent, FUN=sum) + 
+       tapply(groupStage$away_dif, groupStage$away_continent, FUN=sum),
+     decreasing = TRUE)
+
+#3.c.iii
 
 
 
